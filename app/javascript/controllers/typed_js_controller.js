b4071f5f1bfea37ad50aaf275aee070fabf4cc3a
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-09-14 17:59:18 +0200

    add typed js

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from 'typed.js'
+// Connects to data-controller="typed-js"
+export default class extends Controller {
+  connect() {
+    this.typed(
+      
+    )
+  }
+}
