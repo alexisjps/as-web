2aef91d6b8c36340517781d7ccc968136c69461f
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-10-06 14:45:40 +0200

    config

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,27 +1,12 @@
-# Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "https://as-web.website/"
+require 'rubygems'
+require 'sitemap_generator'
 
+SitemapGenerator::Sitemap.default_host = "http://localhost:3000"
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  Defaults: :priority => 0.5, :changefreq => 'weekly',
-            :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-    add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-    Article.find_each do |article|
-      add article_path(article), :lastmod => article.updated_at
-    end
+  add '/', changefreq: 'weekly', priority: 0.9
+  add '/about', changefreq: 'weekly', priority: 0.5
+
+  User.all.each do |user|
+    add user_path(user), lastmod: spot.updated_at
+  end
 end
