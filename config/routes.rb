53d005b6a368ef33365de3d190e42b340b9c9636
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-10-06 14:59:49 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # sitemap
-  resources :sitemaps, only: [:index]
   # Defines the root path route ("/")
   root "lorums#home"
   get "/lorum", to: "lorums#index"
