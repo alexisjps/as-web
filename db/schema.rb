82bf263443d97b05e80fa9cc5c543f242da26d80
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-12-14 23:53:38 +0100

    install invoicing simple
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_06_213643) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_224636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "colors", force: :cascade do |t|
+  create_table "invoices", force: :cascade do |t|
+    t.integer "amount", default: 0
+    t.string "my_company_name"
+    t.string "my_compagny_address"
+    t.integer "my_company_phone"
+    t.string "my_company_email"
+    t.integer "my_company_siret"
+    t.string "my_company_tva"
+    t.text "my_company_paiement"
+    t.string "customer_name"
+    t.string "customer_address"
+    t.string "customer_email"
+    t.integer "customer_siret"
+    t.date "date"
+    t.string "description"
+    t.integer "invoice_number"
+    t.text "prestation"
+    t.integer "invoice_tva"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
