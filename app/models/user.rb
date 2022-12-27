416ba1bdf67c864508bcd05b0615dddfa3f6b6cc
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-12-27 16:46:02 +0100

    dependent destroy
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :invoices
-  has_many :clients
+  has_many :invoices, dependent: :destroy
+  has_many :clients, dependent: :destroy
   has_one_attached :photo
 end
