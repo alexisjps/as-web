463c996c083959f6046d480ec46bc3de0545e090
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-12-29 15:24:45 +0100

    fix problem mailling
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "alexis@astephan.digital"
   layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,7 +14,6 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -64,6 +63,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "generate_lorum_ip_production"
   
   config.action_mailer.perform_caching = false
+  # config mail alexis
+  config.action_mailer.delivery_method = :letter_opener
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: "alexis@astephan.digital",
+    password: ENV['MAILPASSWORD'],
+    authentication: "plain",
+    enable_starttls_auto: true}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
