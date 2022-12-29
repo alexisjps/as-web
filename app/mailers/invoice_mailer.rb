2130e74af00f64ac8cf1b54dee9130dfec6f6ff3
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-12-29 20:35:05 +0100

    design mail envoyé
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -2,6 +2,6 @@ class InvoiceMailer < ApplicationMailer
     def send_invoice(invoice)
         @invoice = invoice
         @user = invoice.user
-        mail(to: @invoice.client.email, subject: "Facture")
+        mail(to: @invoice.client.email, subject: "Facture reçu de #{@user.first_name}")
     end
 end
