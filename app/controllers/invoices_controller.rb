c09d3dc2f256770b2edc2f0f97f60a5a28019392
Alexis Stephan <alexis-stephan@hotmail.fr>
2022-12-30 13:03:25 +0100

    add btn on index for send invoice + redirect to the index when i sended an invoice in the show
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -30,7 +30,7 @@ class InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
     @user = current_user
     InvoiceMailer.send_invoice(@invoice).deliver_now
-    redirect_to invoice_path(@invoice)
+    redirect_to invoices_path
     flash.alert = "Facture envoyée à #{@invoice.client.first_name}"
   end
 
