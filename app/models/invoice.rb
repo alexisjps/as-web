7d9b85983b37e12d8a896ad1a1acd1106cc7ca84
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-03-14 14:04:22 +0100

    psychorigide
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,8 @@
 class Invoice < ApplicationRecord
     belongs_to :user
     belongs_to :client
-    # validates :discount, default: 0
     validates :invoice_tva, presence: true
     validates :amount, presence: true
     validates :date, presence: true
+    # validates :discount, default: 0
 end
