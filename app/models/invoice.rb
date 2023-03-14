6a02e6854df897e8c140d00927e22ee20ccbbb81
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-03-14 14:02:56 +0100

    bug fix
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,7 @@ class Invoice < ApplicationRecord
     belongs_to :user
     belongs_to :client
     # validates :discount, default: 0
+    validates :invoice_tva, presence: true
+    validates :amount, presence: true
+    validates :date, presence: true
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,8 @@
 class Quote < ApplicationRecord
     belongs_to :user
     belongs_to :client
+
+    validates :quote_tva, presence: true
+    validates :amount, presence: true
+    validates :date, presence: true
 end
