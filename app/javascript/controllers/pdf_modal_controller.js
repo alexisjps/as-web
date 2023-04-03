9764611068ce85dd8d48bdddfcf83bb6afe5776a
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-04-03 22:48:59 +0200

    invoice save done
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/javascript/controllers/pdf_modal_controller.js
+++ app/javascript/controllers/pdf_modal_controller.js
@@ -1,20 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
+import { Modal } from "bootstrap";
 
 // Connects to data-controller="pdf-modal"
 export default class extends Controller {
-  static targets = ["iframe", "modal"];
+  static targets = ["modal", "iframe"];
 
   connect() {
-    this.modalElement = this.element.querySelector("#pdfModal");
-    this.modalElement.addEventListener("hidden.bs.modal", () => {
-      this.iframeTarget.src = "";
-    });
+    this.modalInstance = new Modal(this.modalTarget, { backdrop: 'static', keyboard: false });
   }
 
   show(event) {
     event.preventDefault();
-    this.iframeTarget.src = event.currentTarget.dataset.pdfUrl;
-    let myModal = new bootstrap.Modal(this.modalTarget);
-    myModal.show();
+
+    // Get the URL from the data-url attribute
+    const url = event.currentTarget.dataset.url;
+
+    // Set the iframe's src attribute to the URL
+    this.iframeTarget.src = url;
+
+    // Show the Bootstrap modal
+    this.modalInstance.show();
+  }
+
+  hide() {
+    this.modalInstance.hide();
+  }
+
+  // Add this method to clear the iframe's src attribute when the modal is hidden
+  clearIframeSrc() {
+    this.iframeTarget.src = "";
   }
 }
