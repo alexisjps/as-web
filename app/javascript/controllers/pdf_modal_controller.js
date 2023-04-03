14d388aaf63b655513cfdba11c8e5b5926790779
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-04-03 11:16:46 +0200

    fix audio
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/javascript/controllers/pdf_modal_controller.js
+++ app/javascript/controllers/pdf_modal_controller.js
@@ -2,10 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="pdf-modal"
 export default class extends Controller {
-  static targets = ["iframe"];
+  static targets = ["iframe", "modal"];
+
   connect() {
     this.modalElement = this.element.querySelector("#pdfModal");
-    (this.modalElement).on("hidden.bs.modal", () => {
+    this.modalElement.addEventListener("hidden.bs.modal", () => {
       this.iframeTarget.src = "";
     });
   }
@@ -13,6 +14,7 @@ export default class extends Controller {
   show(event) {
     event.preventDefault();
     this.iframeTarget.src = event.currentTarget.dataset.pdfUrl;
-    $(this.modalElement).modal("show");
+    let myModal = new bootstrap.Modal(this.modalTarget);
+    myModal.show();
   }
 }
