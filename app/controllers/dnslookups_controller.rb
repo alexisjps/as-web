100a7d1da9f64ed7152de020beb073e6862f86d9
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-04-05 23:03:58 +0200

    tooltip
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/controllers/dnslookups_controller.rb
+++ app/controllers/dnslookups_controller.rb
@@ -1,24 +1,28 @@
 require 'dnsruby'
+require 'uri'
 
 class DnslookupsController < ApplicationController
-    def lookup
-        domain = params[:domain]
-        
-        if domain.present?
-            record_types = ['A', 'CNAME', 'NS', 'MX', 'TXT']
-            resolver = Dnsruby::Resolver.new
-            @results = {}
-        
-            record_types.each do |record_type|
-            begin
-                response = resolver.query(domain, record_type)
-                @results[record_type] = response.answer.select { |record| record.type.to_s == record_type }
-            rescue Dnsruby::NXDomain
-                @results[record_type] = nil
-            rescue StandardError => e
-                flash.now[:alert] = "Une erreur est survenue lors de la récupération des enregistrements DNS : #{e.message}"
-            end
-            end
+  def lookup
+    domain = params[:domain]
+
+    # Remove protocol and path from the URL, if present
+    domain = URI.parse(domain).host if domain.present?
+
+    if domain.present?
+      record_types = ['A', 'CNAME', 'NS', 'MX', 'TXT']
+      resolver = Dnsruby::Resolver.new
+      @results = {}
+
+      record_types.each do |record_type|
+        begin
+          response = resolver.query(domain, record_type)
+          @results[record_type] = response.answer.select { |record| record.type.to_s == record_type }
+        rescue Dnsruby::NXDomain
+          @results[record_type] = nil
+        rescue StandardError => e
+          flash.now[:alert] = "Une erreur est survenue lors de la récupération des enregistrements DNS : #{e.message}"
         end
+      end
     end
+  end
 end
