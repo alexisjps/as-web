4af122287a05d1b3e6006b6ff716c6e694c50d74
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-06-21 13:57:55 +0200

    add fix for toggle
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -5,7 +5,7 @@ export default class extends Controller {
   static targets = [ "menu" ]
 
   toggle() {
-    console.log("toggle")
     this.menuTarget.classList.toggle("d-none")
+    return;
   }
 }
