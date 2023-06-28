d94d94eda807a1f5a34b5151a0ce0d7cd10e9386
Alexis Stephan <alexis-stephan@hotmail.fr>
2023-06-28 21:15:04 +0200

    policy quote
    Signed-off-by: Alexis Stephan <alexis-stephan@hotmail.fr>

---
+++ app/policies/quote_policy.rb
@@ -0,0 +1,27 @@
+class QuotePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user_id: user.id)
+    end
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    record.user_id == user.id
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user_id == user.id
+  end
+
+  def destroy?
+    record.user_id == user.id
+  end
+end
