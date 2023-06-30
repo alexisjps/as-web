11966baf02cfbef88935001aed7ac6c11f5cc720
Stephan Alexis <62907454+alexisjps@users.noreply.github.com>
2023-06-30 19:17:59 +0200

    Update invoices_controller.rb
    redirect to edit to the send after send the invoice by mail

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -55,7 +55,7 @@ class InvoicesController < ApplicationController
     @user = current_user
     InvoiceMailer.send_invoice(@invoice).deliver_now
     Invoice.update(@invoice.id, status: true)
-    redirect_to invoices_path
+    redirect_to invoices_path(@invoice)
     flash.alert = "Facture envoyée à #{invoice.client.first_name}"
   end
 
