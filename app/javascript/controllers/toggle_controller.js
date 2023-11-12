e62f05f6efc3493b543f91e0357cd1939fca142d
alexis stephan <alexis-stephan@hotmail.fr>
2023-11-12 14:50:57 +0100

    js fix
    Signed-off-by: alexis stephan <alexis-stephan@hotmail.fr>

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,6 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toggle"
 export default class extends Controller {
   static targets = [ "menu" ]
+
   connect(){
     console.log('hello from toggle controller stimulus')
   }
@@ -10,14 +11,19 @@ export default class extends Controller {
     console.log('hello world')
     this.menuTarget.classList.add('active');
     // this.overlayTarget.classList.toggle('active');
-    // if (this.menuTarget.innerText == 'Menu'){
-    //   this.menuTarget.innerText = "Fermer le menu"
-    // } else {
-    //   this.menuTarget = 'Menu'
-    // }
+    if (this.menuTarget.innerText == 'Menu'){
+      this.menuTarget.innerText = "Fermer le menu"
+    } else {
+      this.menuTarget.innerText = 'Menu'
+    }
   }
   disable() {
     this.menuTarget.classList.remove('active');
     // this.overlayTarget.classList.remove('active');
+    if (this.menuTarget.innerText == 'Menu'){
+      this.menuTarget.innerText = "Fermer le menu"
+    } else {
+      this.menuTarget.innerText = 'Menu'
+    }
   }
 }
